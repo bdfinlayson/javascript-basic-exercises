023b41df99ae9a052b0593cf51a29f96a05bd3c2
Bryan Finlayson <bdfinlayson@gmail.com>
2015-02-19 22:30:16 -0600

    added code for exercise 3 for getting the current date and posting it to the page

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ function clear(data) {
   $(data).remove()
 }
 
-
 //Exercise 1
 
 $('#timeButton').on('click', function() {
@@ -21,13 +20,33 @@ function showTime(date) {
   var daysArr = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
   var hours = date.getHours();
   var regHours = hours - 12;
-
-  if (date.getMinutes() < 10) { fixMinutes(date) } else { var minutes = date.getMinutes() }
-
+  var minutes = myMinutes(date);
+  var message = getMessage(date)
   var seconds = date.getSeconds()
-  var el = ('<p id="time">' + "Today is " + daysArr[today] + " and the time is " + regHours + ":" + minutes + " p.m." + " and " + seconds + " seconds." + '</p>')
-
+  var el = ('<p id="time">' + message + "Today is " + daysArr[today] + " and the time is " + regHours + ":" + minutes + " and " + seconds + " seconds." + '</p>')
   $('#ex1').after(el)
+}
+
+function myMinutes(date) {
+  if (date.getMinutes() < 10) {
+     fixMinutes(date)
+  } else {
+     var minutes = date.getMinutes()
+  }
+  return minutes;
+}
+
+function getMessage(date) {
+
+  if (date.getHours() < 12) {
+    var message = 'Good Morning! '
+  } if ( date.getHours() >= 12 && date.getHours() < 18) {
+    var message = 'Good Afternoon! '
+  } if ( date.getHours() >= 18 && date.getHours() <= 24 ) {
+    var message = 'Good Evening! '
+  }
+
+  return message;
 }
 
 function fixMinutes(date) {
@@ -44,3 +63,31 @@ $('#printButton').on('click', function () {
   window.print()
 
 })
+
+
+//Exercise 3
+
+$('#currentDateButton').on('click', function() {
+
+  var date = new Date();
+  showCurrentDate(date)
+
+})
+
+function showCurrentDate(date) {
+
+  var elToClear = '#currentDate'
+  clear(elToClear)
+  var month = date.getMonth()
+  var day = date.getDay() 
+  var year = date.getFullYear()
+  printCurrentDate(month, day, year)
+
+}
+
+function printCurrentDate(month, day, year) {
+
+  var el = ('<p id="currentDate">' + month + '/' + day + '/' + year + '</p>');
+  $('#ex3').after(el)
+
+}
